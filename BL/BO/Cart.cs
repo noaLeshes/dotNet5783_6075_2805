e427ba15c56ec82e161e65e2b6fe2765b1f6220a
Noa <noa.leshes@gmail.com>
2022-12-05 01:24:13 +0200

    cart changes

--- BL/BO/Cart.cs
+++ BL/BO/Cart.cs
@@ -5,7 +5,7 @@ public class Cart
     public string? CostomerName { get; set; }
     public string? CostomerEmail { get; set; }
     public string? CostomerAddress { get; set; }  
-    public IEnumerable<OrderItem?> Items { get; set; } 
+    public IEnumerable<OrderItem>? Items { get; set; } 
     public double TotalPrice { get; set; }
     public override string ToString()
     {
--- BL/BlImplementation/Cart.cs
+++ BL/BlImplementation/Cart.cs
@@ -1,5 +1,9 @@
 ﻿using BlApi;
+using BO;
 using Dal;
+using DalApi;
+using DO;
+
 
 namespace BlImplementation;
 
@@ -8,16 +12,37 @@ internal class Cart : ICart
     DalApi.IDal dal = new Dal.DalList();
    public BO.Cart AddItem(BO.Cart c, int productId)
     {
-           if( DO.OrderItem oi = dal.OrderItem.GetById(productId))
-          {
-            from BO.OrderItem item in c.Items
-            where item.ProductId == productId
-            select 
-          }
-            if(P.InStock > 0)
 
- 
+        BO.OrderItem?oi = (from  item in c.Items
+                           where item.ProductId == productId
+                           select item).FirstOrDefault();
+        DO.Product p = dal.Product.GetById(productId);
 
+        if (oi == null)
+        {
+            if (p.InStock > 0)
+            {
+                c.TotalPrice += p.Price;
+                c.Items.Append(new BO.OrderItem
+                {
+                    Name = p.Name,
+                    ProductId = p.ID,
+                    Amount = 1,
+                    Price = p.Price,
+                    TotalPrice = p.Price
+                });
+            }
+        }
+        else if (p.InStock >= oi.Amount + 1)
+        {
+            oi.Amount++;
+            oi.TotalPrice += oi.Price;//update the total price
+            c.TotalPrice += p.Price;
+        }
+        else
+            throw new Exception("not in stock");
+
+        return c;
     }
 
     public void ConfirmCart(BO.Cart c, string name, string address, string email)
@@ -28,8 +53,34 @@ internal class Cart : ICart
     public BO.Cart UpdateItem(BO.Cart c, int productId, int amount)
     {
         try
-        { DO.Product P = dal.Product.GetById(productId);
-        if(P.InStock == 0)
+        {
+            DO.Product p = dal.Product.GetById(productId);
+            BO.OrderItem? oi = (from item in c.Items
+                                where item.ProductId == productId
+                                select item).First();
+            if (amount == 0)
+            {
+                c.Items.ToList().Remove(oi);   
+                c.TotalPrice -= oi.Price;
+            }
+            else if (amount > oi.Amount)
+            {
+                if (p.InStock >= amount)
+                {
+                    c.TotalPrice += oi.Price * (amount - oi.Amount);
+                    oi.TotalPrice += oi.Price * (amount - oi.Amount);
+                    oi.Amount = amount;
+                }
+                else 
+                    //throw
+            }
+            else if(amount < oi.Amount)
+            {
+                c.TotalPrice -= oi.Price * (oi.Amount - amount);
+                oi.TotalPrice -= oi.Price * (oi.Amount - amount);
+                oi.Amount = amount;
+            }
+        }
 
 
     }
