83398daaaa46e21acd387a0eba4f2e0eafab4f96
hadascarmen@gmail.com <hadascarmen@gmail.com>
2022-12-10 23:24:47 +0200

    windows begining

---
+++ PL/MainWindow.xaml.cs
@@ -0,0 +1,38 @@
+﻿using BlApi;
+using BlImplementation;
+using PL.Windows;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace PL
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            IBl bl = new Bl();
+        }
+
+        private void Products_Click(object sender, RoutedEventArgs e)
+        {
+            ProductListWindow productListWindow = new ProductListWindow();
+            productListWindow.Show();
+        }
+    }
+}
