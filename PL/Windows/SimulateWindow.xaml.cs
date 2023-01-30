ab65714f141af7a01caf8b60bcc93a9f5c021fbc
hadascarmen@gmail.com <hadascarmen@gmail.com>
2023-01-30 13:22:51 +0200

    simulation updates

--- PL/Windows/SimulateWindow.xaml.cs
+++ PL/Windows/SimulateWindow.xaml.cs
@@ -14,6 +14,8 @@ using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Shapes;
+using BO;
+using Simulator;
 
 namespace PL.Windows
 {
@@ -22,13 +24,21 @@ namespace PL.Windows
     /// </summary>
     public partial class SimulateWindow : Window
     {
+        static int timerNum = 0;
+        string msg;
+        private Stopwatch stopWatch;
         BackgroundWorker bw;
-        Stopwatch sw = new Stopwatch();
+        int id=0;
+        OrderStatus before;
+        DateTime begin;
+        OrderStatus after;
+        DateTime end;
+
         public SimulateWindow()
         {
             InitializeComponent();
             bw = new BackgroundWorker();
-            //stopwatch = new Stopwatch();
+            stopWatch = new Stopwatch();
             bw.DoWork += Worker_DoWork;
             bw.ProgressChanged += Worker_ProgressChanged;
             bw.RunWorkerCompleted += Worker_RunWorkerCompleted;
@@ -37,32 +47,54 @@ namespace PL.Windows
             bw.WorkerSupportsCancellation = true;
             bw.RunWorkerAsync();
         }
-        public 
         private void Worker_DoWork(object sender, DoWorkEventArgs e)
         {
-
-            //Simulation.report1+= e.Argument;
-            //Simulation.Activate();
-            while(bw.CancellationPending==false)
+            Simulator.Simulator.Activate();
+            Simulator.Simulator.Register1(DoR1);
+            Simulator.Simulator.Register2(DoR2);
+            Simulator.Simulator.Register3(DoR3);
+            while (bw.CancellationPending==false)
             {
                 Thread.Sleep(1000);
-                bw.ReportProgress(0);
             }
         }
         private void Worker_ProgressChanged(object sender, ProgressChangedEventArgs e)
         {
-            //watch
-           int p= e.ProgressPercentage;
-            lblResult.Content = p + "%";
-            progresBar.Value = p;
-            txtClock.Text = p.ToString();
+            string timerText = stopWatch.Elapsed.ToString();
+            timerText = timerText.Substring(0, 8);
+            txtClock.Text = timerText;
+            switch (e.ProgressPercentage)
+            {
+                case 0:
+                    txtId.Text = id.ToString();
+                    txtFirstStatus.Text = before.ToString();
+                    txtSecondStatus.Text = after.ToString();
+                    timerNum++;
+                    lblResult.Content = timerNum+"%";
+                    progresBar.Value++;
+                    break;
+                case 1:
+                    break;
+                case 2:
+                    MessageBox.Show(msg, " 😃 ", MessageBoxButton.OK, MessageBoxImage.None);// a messagebox appears when the product is added
+                    break;
+
+
+            }
+            //e.UserState
+
+           
+            
         }
         private void Worker_RunWorkerCompleted(object sender, RunWorkerCompletedEventArgs e)
         {
-
+            Simulator.Simulator.UnRegister1(DoR1);
+            Simulator.Simulator.UnRegister2(DoR2);
+            Simulator.Simulator.UnRegister3(DoR3);
         }
         private void btnStart_Click(object sender, RoutedEventArgs e)
         {
+            stopWatch.Restart();
 
         }
 
@@ -72,6 +104,26 @@ namespace PL.Windows
             {
                 bw.CancelAsync();
             }
+            this.Close();
+        }
+        public void DoR1(Order o, OrderStatus first, DateTime b, OrderStatus second, DateTime e)
+        {
+            id = o.ID;
+            before = first;
+            begin = b;
+            after = second;
+            end = e;
+            bw.ReportProgress(0);
+        }
+        public void DoR2(string s)
+        {
+            msg = s +id ;
+            bw.ReportProgress(1);
+        }
+        public void DoR3(string s)
+        {
+            msg = s;
+            bw.ReportProgress(2);
         }
     }
 }
